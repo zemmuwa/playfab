952d936a57aa205b4d20415789fb131ea006043f
zemmuwa <48934454+zemmuwa@users.noreply.github.com>
2024-12-20 15:24:02 +0700

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -1,1 +1,15 @@
+handlers.grantItemsToUser = function (args, context) {
+    try {
+        
+        var grantItemRequest = {PlayFabId:currentPlayerId,CatalogVersion:args.catalogVersion,ItemIds:itemsIds};
+        // var playerStatResult = server.GrantItemsToUser(request);
+        return {success:true,data:args};
+    } catch (ex) {
+        server.WriteTitleEvent({
+            EventName : 'cs_error',
+            Body : ex
+        });
+        return ex;
+    }
+}
 
